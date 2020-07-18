a4d1c797c4328e2b86bc6c015a06d5499b053a05
vrudas <vrudas4work@gmail.com>
2020-07-18 21:11:31 +0300

    Implemented restart of game by pressing on SPACE

--- Levels/GameOver.gd
+++ Levels/GameOver.gd
@@ -1,9 +1,16 @@
 extends Control
 
 
-func _on_RestartButton_pressed():
-    get_tree().change_scene("res://Levels/Level1.tscn")
+func _on_RestartButton_pressed() -> void:
+    restart_game()
 
-func _process(delta):
+func _process(delta) -> void:
     if Input.is_key_pressed(KEY_ESCAPE):
         get_tree().quit()
+
+    if Input.is_key_pressed(KEY_SPACE):
+        restart_game()
+
+
+func restart_game() -> void:
+    get_tree().change_scene("res://Levels/Level1.tscn")
